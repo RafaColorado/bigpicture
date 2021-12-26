02e4b2e54b0b48695d9118069b3a153026865cd0
RafColorado <49199772+RafaColorado@users.noreply.github.com>
2021-12-26 15:40:01 -0800

    Update Startup.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-
+// code change4
 namespace Tailwind.Traders.Web
 {
     public class Startup
